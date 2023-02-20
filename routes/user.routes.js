ccc293f1747195919abe1a83fa6add494627720d
vaibhzz101 <vaibhzzsmiley@gmail.com>
2023-02-20 17:35:29 +0530

    backend

---
+++ routes/user.routes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const controller = require('../controllers/user.controller')
+const userRoute = express.Router()
+
+userRoute.post('/register', controller.register)
+userRoute.post('/login', controller.login)
+
+module.exports={
+    userRoute
+}
